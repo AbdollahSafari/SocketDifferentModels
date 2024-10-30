3081b5e788cac400eb8414b37ea310659d3dfb25
Abdollah Safari <abdollahsafari1986@gmail.com>
2024-10-30 10:00:48 +0330

    Start Project

---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace SocketDifferentModels;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        //****************************************Async Mode*************************************
+        //UdpListener udpListener = new UdpListener(IPAddress.Parse("120.110.1.11"), 9090);
+        //udpListener.StartListen();
+        //udpListener.StartReceive();
+
+        //Console.ReadLine();
+
+
+        //*******************************************Pooling***********************************
+        //var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //socket.Bind(new IPEndPoint(IPAddress.Parse("120.110.1.11"),9090));
+        //while (true)
+        //{
+        //    if (socket.Available > 0)
+        //    {
+        //        var buffer = new byte[socket.Available];
+        //        EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+        //        var readCout = socket.ReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endPoint);
+        //        Console.WriteLine($"Data Received From : {endPoint}, {Encoding.ASCII.GetString(buffer, 0, readCout)}");
+        //    }
+        //}
+
+        //****************************************Async Await*************************************
+
+        UdpListener udpListener = new UdpListener(IPAddress.Parse("120.110.1.11"), 9090);
+        udpListener.StartListen();
+        udpListener.StartReceiveAsync();
+        while (true)
+        {
+            var line = Console.ReadLine();
+            udpListener.SendData(line);
+        }
+    }
+}
---
+++ src/UdpListener.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketDifferentModels;
+
+public class UdpListener
+{
+    private readonly IPAddress _ipAddress;
+    private readonly int _port;
+    private readonly byte[] _buffer = new byte[1000];
+    private EndPoint _endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+    public UdpListener(IPAddress ipAddress, int port)
+    {
+        _ipAddress = ipAddress;
+        _port = port;
+    }
+
+    private Socket? _socket;
+    public void StartListen()
+    {
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        _socket.Bind(new IPEndPoint(_ipAddress, _port));
+    }
+
+    public void StartReceive()
+    {
+        _socket.BeginReceiveFrom(_buffer, 0, _buffer.Length, SocketFlags.None, ref _endPoint, ReceiveCallBack, null);
+    }
+
+    private void ReceiveCallBack(IAsyncResult ar)
+    {
+        if (ar.IsCompleted)
+        {
+            var receiveCount = _socket.EndReceiveFrom(ar, ref _endPoint);
+            Console.WriteLine($"Data Received From : {_endPoint}, {Encoding.ASCII.GetString(_buffer, 0, receiveCount)}");
+            StartReceive();
+        }
+    }
+
+    public async Task StartReceiveAsync()
+    {
+        while (true)
+        {
+            var result = await _socket.ReceiveFromAsync(new Memory<byte>(_buffer), _endPoint);
+            Console.WriteLine(
+                $"Data Received From : {result.RemoteEndPoint}, {Encoding.ASCII.GetString(_buffer, 0, result.ReceivedBytes)}");
+        }
+    }
+
+    public void SendData(string data)
+    {
+        _socket.SendTo(Encoding.ASCII.GetBytes(data), SocketFlags.None,
+            new IPEndPoint(IPAddress.Parse("120.110.1.12"), 5452));
+    }
+}
